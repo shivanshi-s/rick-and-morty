4b4d6c94deeef3c72bdf0219169eca66c4fdcda2
shivanshi <shivanshi.saxena2@gmail.com>
2024-04-01 01:59:15 +0530

    Search implementation

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,16 +4,40 @@ import Footer from './components/Footer'
 import Input from './components/Input'
 import Button from './components/Button'
 import CardsList from './components/CardsList'
+import Error from './components/Error'
 
 const Home = () => {
 
     const [ characters, setCharacters] = useState();
+    const [initData, setInitData] = useState();
+    const [searchTermCharacters, setSearchTermCharacters] = useState();
+    const [noResults, setNoResults] = useState(false);
+    const [totalResults, setTotalResults] = useState();
+
+    const searchTerm = (e) => {
+        setSearchTermCharacters(e.target.value);
+        searchCharacters()
+    }
+
+    const searchCharacters = () => {
+        if(searchTermCharacters) {
+            const searchResults = characters.filter((character) =>
+                character.name.toLowerCase().includes(searchTermCharacters.toLowerCase())
+            )
+            setCharacters(searchResults)
+            setNoResults(searchResults.length === 0)
+        } else {
+            setCharacters(initData)
+        }
+    }
 
     const getCharacters = async () => {
         try {
             const data = await fetch("https://rickandmortyapi.com/api/character");
             const response = await data.json();
             setCharacters(response.results);
+            setInitData(response.results);
+            setTotalResults(response.info.count);
         }
         catch (err) {
             console.log(err);
@@ -27,8 +51,9 @@ const Home = () => {
 
     return <section>
         <Header />
-            <Input type="search" placeholder="type your character" changeHandler={() => {}} />
-            <CardsList characters={characters} />
+            <Input type="search" placeholder="type your character" changeHandler={searchTerm} />
+            { totalResults && <p className='mt-4'>Total Characters are : <span className='font-black'>{totalResults}</span> </p>}
+            {noResults ? <Error/> : <CardsList characters={characters} />}
             <Button clickHandler={() => {}}>Next</Button>
         <Footer />
     </section>
---
+++ src/components/Error.jsx
@@ -0,0 +1,5 @@
+const Error = () => {
+    return <p className="text-xl text-red-400 p-4">Error</p>
+}
+
+export default Error;
