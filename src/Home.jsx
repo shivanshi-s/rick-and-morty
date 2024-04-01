663802368903c1f98b85bc563486ce54eceda098
shivanshi <shivanshi.saxena2@gmail.com>
2024-04-01 14:48:35 +0530

    pagination completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,6 +13,8 @@ const Home = () => {
     const [searchTermCharacters, setSearchTermCharacters] = useState();
     const [noResults, setNoResults] = useState(false);
     const [totalResults, setTotalResults] = useState();
+    const [totalPages, setTotalPages] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
 
     const searchTerm = (e) => {
         setSearchTermCharacters(e.target.value);
@@ -31,13 +33,15 @@ const Home = () => {
         }
     }
 
-    const getCharacters = async () => {
+    const getCharacters = async (page=1) => {
         try {
-            const data = await fetch("https://rickandmortyapi.com/api/character");
+            const data = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
             const response = await data.json();
+            console.log(response)
             setCharacters(response.results);
             setInitData(response.results);
             setTotalResults(response.info.count);
+            setTotalPages(response.info.pages);
         }
         catch (err) {
             console.log(err);
@@ -49,12 +53,24 @@ const Home = () => {
 
     },[])
 
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+        getCharacters(newPage);
+    }
+
     return <section>
         <Header />
             <Input type="search" placeholder="type your character" changeHandler={searchTerm} />
             { totalResults && <p className='mt-4'>Total Characters are : <span className='font-black'>{totalResults}</span> </p>}
             {noResults ? <Error/> : <CardsList characters={characters} />}
-            <Button clickHandler={() => {}}>Next</Button>
+            {totalPages && 
+                <div className='flex gap-2 my-4 justify-center'>
+                <Button classes="bg-black text-white p-2 rounded-md" clickHandler={() => handlePageChange(currentPage - 1)} buttonLabel="Prev"
+                disabled={currentPage === 1} />
+                    <p>Page {currentPage} of {totalPages}</p>
+                <Button classes="bg-black text-white p-2 rounded-md" clickHandler={() => handlePageChange(currentPage + 1)} buttonLabel="Next"
+                disabled={currentPage === totalPages} />
+                </div> }
         <Footer />
     </section>
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line react/prop-types
-const Button = ({buttonLabel , clickHandler}) => {
-    return <button onClick={clickHandler}>{buttonLabel}</button>
+const Button = ({buttonLabel , clickHandler, classes, disabled }) => {
+    return <button disabled={disabled} className={`${classes}`} onClick={clickHandler}>{buttonLabel}</button>
 }
 
 export default Button;
